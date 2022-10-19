6dd2220eb61771192259d907e272a0ea024711c7
farya <ftjamal2017@gmail.com>
2022-10-20 04:40:49 +0500

    some changes

--- web/src/context/Context.js
+++ web/src/context/Context.js
@@ -7,7 +7,7 @@ let data = {
   user: {},
   darkTheme: true,
   myNum : 5,
-  baseUrl: (window.location.href.indexOf("https") === -1) ? "http://localhost:5001" : "https://repulsive-threads-fawn.cyclic.app"}
+  baseUrl: (window.location.href.indexOf("https") === -1) ? "http://localhost:5001" : "https://long-cyan-pigeon-kilt.cyclic.app"}
 
 
 export default function ContextProvider({ children }) {
